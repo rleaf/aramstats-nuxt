06b91f1819ac31cf326330e02335e25d5562c4f6
Ryan <calcifer44@gmail.com>
2025-03-06 01:03:07 -0500

    v4.0.1 gtag fixes

--- plugins/gtm.client.js
+++ plugins/gtm.client.js
@@ -1,18 +1,16 @@
 export default defineNuxtPlugin((nuxtApp) => {
-   if (import.meta.env.PROD) {
-      (function (w, d, s, l, i) {
-         w[l] = w[l] || []
-         w[l].push({
-            'gtm.start':
-               new Date().getTime(),
-            event: 'gtm.js'
-         })
-         const f = d.getElementsByTagName(s)[0]
-         const j = d.createElement(s); const dl = l !== 'dataLayer' ? '&l=' + l : ''
-         j.async = true
-         j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-         f.parentNode.insertBefore(j, f)
-      })(window, document, 'script', 'dataLayer', import.meta.env.GTM_KEY)
-   }
+   (function (w, d, s, l, i) {
+      w[l] = w[l] || []
+      w[l].push({
+         'gtm.start':
+            new Date().getTime(),
+         event: 'gtm.js'
+      })
+      const f = d.getElementsByTagName(s)[0]
+      const j = d.createElement(s); const dl = l !== 'dataLayer' ? '&l=' + l : ''
+      j.async = true
+      j.src =
+         'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+      f.parentNode.insertBefore(j, f)
+   })(window, document, 'script', 'dataLayer', 'G-PD6QYJ923C')
 })
